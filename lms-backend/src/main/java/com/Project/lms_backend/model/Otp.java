e6a3fc93bc97d4af9455651b6808e0213416df72
Chinnam Eswar <chinnam.eswar@ltim.trimio.ai>
2025-07-23 21:32:28 +0530

    initial comit with update the otp model and document model

--- lms-backend/src/main/java/com/Project/lms_backend/model/Otp.java
+++ lms-backend/src/main/java/com/Project/lms_backend/model/Otp.java
@@ -1,4 +1,37 @@
 package com.Project.lms_backend.model;
-public class LoanProduct {
 
-}
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "otps")
+@Data
+@NoArgsConstructor
+public class Otp {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    @Column(name = "otp_code")
+    private String otpCode;
+
+    private String type; // e.g., "LOGIN_VERIFICATION"
+
+    private boolean used = false;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime expiresAt;
+} 
